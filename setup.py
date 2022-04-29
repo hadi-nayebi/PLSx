32d6bcce03e880e8baac57793f8c066c7f09524a
hadinayebi <hadinayebi@gmail.com>
2022-04-29 08:51:42 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# by hadinayebi@gmail.com
+__version__ = "0.1.0"
+
+# from Plsx 0.1.0
+# run this command to update module
+# pip install -e .
+
+
+# imports
+from setuptools import find_packages, setup
+
+setup(name="Plsx", version=__version__, packages=find_packages())
