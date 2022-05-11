055ea7e3dab63854b96cd592f4e91666cefb39af
hadinayebi <hadinayebi@gmail.com>
2022-05-11 02:20:50 -0400

    creating a simple view

--- PLSxView/dataView/models.py
+++ PLSxView/dataView/models.py
@@ -8,7 +8,7 @@ class Protein(models.Model):
 
     uniprot_id = models.CharField(max_length=20, unique=True, primary_key=True)
     sequence = models.TextField()
-    secondary_structure = models.TextField()
+    secondary_structure = models.TextField(blank=True)
 
     def __str__(self):
         """Return the string representation of the protein."""
--- PLSxView/dataView/views.py
+++ PLSxView/dataView/views.py
@@ -1,6 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+from .models import Protein
+
+# def index(request):
+#     protein_list = Protein.objects.order_by("uniprot_id")
+#     template = loader.get_template("dataView/index.html")
+#     context = {"protein_list": protein_list}
+#     return HttpResponse(template.render(context, request))
 
 
 def index(request):
-    return HttpResponse("Data View")
+    protein_list = Protein.objects.order_by("uniprot_id")
+    context = {"protein_list": protein_list}
+    return render(request, "dataView/index.html", context)
+
+
+def protein(request, uniprot_id):
+    return HttpResponse("Protein View: {}".format(uniprot_id))
