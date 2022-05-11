4e758b9d4f833469e6d8f235ea981d0b8c0491b9
hadinayebi <hadinayebi@gmail.com>
2022-05-11 01:49:09 -0400

    set  up db

--- PLSxView/dataView/models.py
+++ PLSxView/dataView/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from sqlalchemy import ForeignKey
 
-# Create your models here.
+
+class Protein(models.Model):
+    """
+    A class to represent a protein."""
+
+    uniprot_id = models.CharField(max_length=20, unique=True, primary_key=True)
+    sequence = models.TextField()
+    secondary_structure = models.TextField()
+
+    def __str__(self):
+        """Return the string representation of the protein."""
+        return f"{self.uniprot_id} -> \nAAS: {self.sequence}\nSSS: {self.secondary_structure}"
+
+
+class Annotations(models.Model):
+    """
+    A class to represent annotations."""
+
+    uniprot_id = models.CharField(max_length=20)
+    annotation = models.TextField()
+    start = models.IntegerField()
+    end = models.IntegerField()
