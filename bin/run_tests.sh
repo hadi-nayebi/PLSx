d852782f6b8b0c643040b39be0d0298cdab418f9
hadinayebi <hadinayebi@gmail.com>
2022-05-23 21:05:03 -0400

    adding new tests

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -1,7 +1,10 @@
 cd /Users/hadinayebi/CodingProjects/plsx/
-coverage run ./PLSx/dataloader/test/test_dataloader.py
+coverage run ./PLSx/utils/test/test_template.py
+coverage run -a ./PLSx/dataloader/test/test_dataloader.py
 coverage run -a ./PLSx/database/test/test_sqlitedb.py
 coverage run -a ./PLSx/dataloader/test/test_utils.py
 coverage run -a ./PLSx/autoencoder/test/test_architecture.py
+coverage run -a ./PLSx/utils/test/test_custom_argparser.py
+coverage run -a ./PLSx/autoencoder/test/test_autoencoder.py
 coverage report -m
 coverage html
