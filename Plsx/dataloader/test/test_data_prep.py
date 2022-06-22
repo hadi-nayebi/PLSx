ac71776ce076018b0d5f36f640c2003ba24fbd71
hadinayebi <hadinayebi@gmail.com>
2022-06-22 12:06:00 -0400

    update test file

--- Plsx/dataloader/test/test_data_prep.py
+++ Plsx/dataloader/test/test_data_prep.py
@@ -2,13 +2,7 @@
 # coding: utf-8
 
 """Unit test for DataPrep."""
-
-import sys
 from pathlib import Path
-
-# sys.path.insert(1, Path(__file__).resolve().parents[3])
-# print(Path(__file__).resolve().parents[3])
-# print(sys.path)
 from unittest import TestCase
 from unittest import main as unittest_main
 
@@ -24,8 +18,12 @@ class TestDataPrep(TestCase):
     def test_load_source(self):
         """Test load_source."""
         data_prep = DataPrep()
-        source = self.root / "data" / "uniprot" / "UP000000212" / "UP000000212_1234679.xml"
-        source = self.root / "data" / "uniprot" / "UP000000212" / "UP000000212_1234679.xml"
+        # source = (
+        #     self.root / "data" / "uniprot" / "UP000000212" / "UP000000212_1234679.xml"
+        # )
+        source = self.root / "data" / "uniprot" / "uniprot_sprot.xml"
+        if not source.exists():
+            self.skipTest("Source file not found.")
         data_prep.load_source(source)
 
 
